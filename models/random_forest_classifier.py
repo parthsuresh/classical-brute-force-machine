abc40f6377f5588f3a14f4ae8462fa9c0dba61e3
parthsuresh <parthsur@usc.edu>
2020-06-27 23:20:14 -0700

    Corrected RFC

--- models/random_forest_classifier.py
+++ models/random_forest_classifier.py
@@ -10,6 +10,7 @@ import numpy as np
 from metrics.classification_metrics import f1
 from metrics.classification_metrics import roc
 from metrics.classification_metrics import auc
+from metrics.classification_metrics import balanced_accuracy
 from metrics.classification_metrics import accuracy
 
 class RandomForestClassificationModel():
@@ -48,44 +49,44 @@ class RandomForestClassificationModel():
                                                                         min_samples_split=min_samples_split,
                                                                         min_samples_leaf=min_samples_leaf,
                                                                         max_features=max_features)
-                            model_parameters = {
+                            model_params = {
                                 'n_estimators':n_estimators,
                                 'min_samples_split':min_samples_split,
                                 'min_samples_leaf':min_samples_leaf,
                                 'max_features':max_features
                             }
-                            ext_estimator.fit(X_train, y_train)
+                            ext_estimator.fit(X_train, y_train.values.ravel())
                             preds = ext_estimator.predict(X_val)
 
                             if model_selection_metric == "f1":
-                                        f = f1(y_val, preds)
-                                        best_f1_run, best_model_run = (f, gbm_estimator) if f > best_f1_run else (
-                                            best_f1_run, best_model_run)
-                                        best_f1, best_model, best_model_params = (f, gbm_estimator, model_params) if f > best_f1 else (
-                                            best_f1, best_model, best_model_params)
+                                f = f1(y_val, preds)
+                                best_f1_run, best_model_run = (f, ext_estimator) if f > best_f1_run else (
+                                    best_f1_run, best_model_run)
+                                best_f1, best_model, best_model_params = (f, ext_estimator, model_params) if f > best_f1 else (
+                                    best_f1, best_model, best_model_params)
                             elif model_selection_metric == "roc":
                                 r = roc(y_val, preds)
-                                best_roc_run, best_model_run = (r, gbm_estimator) if r > best_roc_run else (
+                                best_roc_run, best_model_run = (r, ext_estimator) if r > best_roc_run else (
                                     best_roc_run, best_model_run)
-                                best_roc, best_model, best_model_params = (r, gbm_estimator, model_params) if r > best_roc else (
+                                best_roc, best_model, best_model_params = (r, ext_estimator, model_params) if r > best_roc else (
                                     best_roc, best_model, best_model_params)
                             elif model_selection_metric == "auc":
                                 au = auc(y_val, preds)
-                                best_auc_run, best_model_run = (au, gbm_estimator) if au > best_auc_run else (
+                                best_auc_run, best_model_run = (au, ext_estimator) if au > best_auc_run else (
                                     best_auc_run, best_model_run)
-                                best_auc, best_model, best_model_params = (au, gbm_estimator, model_params) if au > best_auc else (
+                                best_auc, best_model, best_model_params = (au, ext_estimator, model_params) if au > best_auc else (
                                     best_auc, best_model, best_model_params)
                             elif model_selection_metric == "accuracy":
                                 acc = accuracy(preds, y_val)
-                                best_acc_run, best_model_run = (acc, gbm_estimator) if acc > best_acc_run else (
+                                best_acc_run, best_model_run = (acc, ext_estimator) if acc > best_acc_run else (
                                     best_acc_run, best_model_run)
-                                best_acc, best_model, best_model_params = (acc, gbm_estimator, model_params) if acc > best_acc else (
+                                best_acc, best_model, best_model_params = (acc, ext_estimator, model_params) if acc > best_acc else (
                                     best_acc, best_model, best_model_params)
                             elif model_selection_metric == "balanced_accuracy":
                                 bal_acc = balanced_accuracy(preds, y_val)
-                                best_bal_acc_run, best_model_run = (bal_acc, gbm_estimator) if bal_acc > best_bal_acc_run else (
+                                best_bal_acc_run, best_model_run = (bal_acc, ext_estimator) if bal_acc > best_bal_acc_run else (
                                     best_bal_acc_run, best_model_run)
-                                best_bal_acc, best_model, best_model_params = (bal_acc, gbm_estimator, model_params) if bal_acc > best_bal_acc else (
+                                best_bal_acc, best_model, best_model_params = (bal_acc, ext_estimator, model_params) if bal_acc > best_bal_acc else (
                                     best_bal_acc, best_model, best_model_params)
                             else:
                                 print("Wrong model selection metric entered!")
@@ -121,12 +122,6 @@ class RandomForestClassificationModel():
 
         f = open(plots_path + "feature_importances.txt", "w")
         f.write("Feature Importances\n")
-        for i in reversed(indices):
-            f.write(str(features[i]) + " : "  + str(importances[i]) + "\n")
-        f.close()
-
-
-
     @classmethod
     def record_scores(self, X_test, y_test, n_runs, metrics, feature_names, results_path):
         models_scores_path = results_path + '/model_scores/'
@@ -134,17 +129,24 @@ class RandomForestClassificationModel():
         workbook = xlsxwriter.Workbook(models_scores_path+'random_forest_results.xlsx')
         worksheet = workbook.add_worksheet()
 
+        best_f1 = 0
+        best_acc = 0
+        best_roc = 0
+        best_auc = 0
+        best_bal_acc = 0
+        best_model = None
+
         row, column = 0, 0
         worksheet.write(row, column, "Model Name")
 
         f = open(models_scores_path+"metric.txt", "a")
 
         for n in range(n_runs):
-            model_path = results_path + '/gbm_models/gbm_model_' + str(n) + '.sav'
+            model_path = results_path + '/random_forest_models/rf_model_' + str(n) + '.sav'
             preds = self.predict(X_test, results_path, n)
-            f.write("GBM Model " + str(n) + "\t")
+            f.write("RF Model " + str(n) + "\t")
             row = n + 1
-            worksheet.write(row, 0, "GBM Model " + str(n) + "\t")
+            worksheet.write(row, 0, "RF Model " + str(n) + "\t")
 
             column = 0
 
